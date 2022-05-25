a6885d46c2563cc0d2171d730f42b71552c869ea
Karlo Vardijan <kvardijan20@foi.hr>
2022-05-25 15:43:46 +0200

    HousingUnitRepository i GuestRepository dodani.

---
+++ Campsite Manager/Repositories/GuestRepository.cs
@@ -0,0 +1,50 @@
+﻿using Campsite_Manager.Models;
+using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campsite_Manager.Repositories
+{
+    public class GuestRepository
+    {
+        public static Guest GetGuest(int id)
+        {
+            Guest guest = null;
+            string sql = $"SELECT * FROM Gosti WHERE ID = {id}";
+
+            DB.SetConfiguration("kvardijan20_DB", "kvardijan20", "%J6i}G!Z");
+            DB.OpenConnection();
+
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                guest = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+
+            return guest;
+        }
+
+        private static Guest CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["ID"].ToString());
+            string firstName = reader["Ime"].ToString();
+            string lastName = reader["Prezime"].ToString();
+
+            var guest = new Guest
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            return guest;
+        }
+    }
+}
---
+++ Campsite Manager/Repositories/HousingUnitRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Campsite_Manager.Models;
+using DBLayer;
+
+namespace Campsite_Manager.Repositories
+{
+    public class HousingUnitRepository
+    {
+        public static HousingUnit GetHousingUnit(int id)
+        {
+            HousingUnit housingUnit = null;
+            string sql = $"SELECT * FROM Smjestajne_jedinice WHERE ID = {id}";
+
+            DB.SetConfiguration("kvardijan20_DB", "kvardijan20", "%J6i}G!Z");
+            DB.OpenConnection();
+
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                housingUnit = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+
+            return housingUnit;
+        }
+
+        public static List<HousingUnit> GetHousingUnits()
+        {
+            List<HousingUnit> housingUnits = new List<HousingUnit>();
+            string sql = "SELECT * FROM Smjestajne_jedinice";
+
+            DB.SetConfiguration("kvardijan20_DB", "kvardijan20", "%J6i}G!Z");
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+
+            while (reader.Read())
+            {
+                HousingUnit housingUnit = CreateObject(reader);
+                housingUnits.Add(housingUnit);
+            }
+            reader.Close();
+            DB.CloseConnection();
+
+            return housingUnits;
+        }
+
+        private static HousingUnit CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["ID"].ToString());
+            string unitName = reader["Naziv"].ToString();
+            int unitType = int.Parse(reader["Vrsta"].ToString());
+            int maxCapacity = int.Parse(reader["MaxKapacitet"].ToString());
+
+            var housingUnit = new HousingUnit
+            {
+                Id = id,
+                UnitName = unitName,
+                UnitType = unitType.ToString(),
+                MaxCapacity = maxCapacity,
+            };
+
+            return housingUnit;
+        }
+    }
+}
